d7b89582069a41fc51f9c626704805323d0e0613
kirah-cox <kirahanncox@gmail.com>
2026-09-11 11:32:54 -0600

    changed products table

---
+++ BackendCommerceApp/Migrations/20260911172551_AddProductCategory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendCommerceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"BackendCommerceApp\".\"Products\";");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                schema: "BackendCommerceApp",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                schema: "BackendCommerceApp",
+                table: "Products");
+        }
+    }
+}
