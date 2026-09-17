1ea961a309c65dbf767c2fd3f19ce4de352d5e55
Kyler O <kyler.olsen@students.snow.edu>
2026-09-17 13:41:57 -0600

    Added User Orders Page

--- BackendCommerceApp/Models/Order.cs
+++ BackendCommerceApp/Models/Order.cs
@@ -3,6 +3,7 @@ namespace BackendCommerceApp.Models;
 public class Order
 {
     public int OrderId { get; set; }
+    public int? UserId { get; set; }
     public DateTime Date { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
